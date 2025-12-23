526d5dee579ddfa0c6a44b30f0cb973729aeb454
ramakanthkyosk <ramakanth.rapaka@kyosk.app>
2025-12-23 19:20:26 +0530

    Initial commit - Laravel project setup

--- resources/views/partials/dashboard/header.blade.php
+++ resources/views/partials/dashboard/header.blade.php
@@ -1,9 +1,12 @@
 <div class="header">
     <nav class="navbar navbar-expand d-flex">
 
-        <button id="sidebarToggleTop" class="btn btn-link nav-off rounded-circle mr-3">
-            ☰
-        </button>
+        <button id="sidebarToggleTop" class="btn btn-link nav-off rounded-circle mr-3"><svg
+                xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#0b5ed7" class="bi bi-list"
+                viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                    d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
+            </svg></button>
 
         <a class="navbar-brand d-lg-none d-block bg-white col-md-2 col-3 py-2" href="{{ route('dashboard') }}">
             <img src="{{ asset('img/logo.png') }}" class="img-fluid" />
@@ -13,8 +16,7 @@
             <li class="nav-item dropdown">
                 <button class="btn dropdown-toggle" data-bs-toggle="dropdown">
                     <span class="me-2">{{ auth()->user()->name }}</span>
-                    <img class="img-profile rounded-circle" width="32"
-                         src="{{ asset('img/undraw_profile.svg') }}">
+                    <img class="img-profile rounded-circle" width="32" src="{{ asset('img/undraw_profile.svg') }}">
                 </button>
 
                 <ul class="dropdown-menu dropdown-menu-end">
